859f6eea511b2d6ba481aea3ac4a788c45a5d5f2
2md-mahmudul-hasan <2md.mahmudul.hasan@gamil.com>
2023-04-27 07:47:16 +0600

    news

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const cors = require('cors')
 const port = 5000;
 app.use(cors())
 const catagories = require('./data/data.json')
+const news = require('./data/news.json')
+console.log(news)
 app.get('/', (req, res)=>{
   res.send('app i running')
 })
@@ -12,7 +14,21 @@ app.get('/', (req, res)=>{
 app.get('/catagories', (req, res)=>{
   res.send(catagories)
 })
+app.get('/catagories/:id', (req, res)=>{
+  const id = req.params.id;
+  const catagoriesNews = news.filter(n=>n.category_id === id)
+  res.send(catagoriesNews)
+})
+
+app.get('/news',(req, res)=>{
+  res.send(news)
+})
+app.get('/news/:id',(req, res)=>{
+  const id = req.params.id;
+  const selectedNews = news.find(n=>n._id==id)
+  res.send(selectedNews)
+})
 
 app.listen(5000, ()=>{
-  console.log('app is running on server 5000')
+  console.log('app is running on  server 5000')
 })
